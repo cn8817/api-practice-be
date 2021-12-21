c2b6423ae3a068dbbd83b807af5c487d3d92fca0
cn8817 <christinenguyen1433@gmail.com>
2021-12-20 20:28:49 -0800

    made all files and folders and made comments dummy data

---
+++ api/comments/comments-data.js
@@ -0,0 +1,18 @@
+const comments = [
+    {
+        "id": "123456",
+        "comment": "I love this movie"
+    },
+    {
+        "id": "123457",
+        "comment": "Best movie ever"
+    },
+    {
+        "id": "123458",
+        "comment": "Not as good as I thought"
+    },
+    {
+        "id": "123459",
+        "comment": "Princess Mononoke was better tbh"
+    },
+]
